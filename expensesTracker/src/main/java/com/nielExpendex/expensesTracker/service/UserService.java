2365384ba2c7bf02e02855593ac36f6167b1e825
danieBuilds <daniebuilds@gmail.com>
2026-07-23 11:20:56 +0100

    new imports

--- expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/UserService.java
+++ expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/UserService.java
@@ -5,6 +5,10 @@ import com.nielExpendex.expensesTracker.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
