229b89b6a0f0b671a2866a9cf09878f93e074935
danieBuilds <daniebuilds@gmail.com>
2026-07-23 11:21:09 +0100

    new import

--- expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/UserService.java
+++ expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/UserService.java
@@ -2,6 +2,7 @@ package com.nielExpendex.expensesTracker.service;
 
 import com.nielExpendex.expensesTracker.model.Users;
 import com.nielExpendex.expensesTracker.repository.UserRepo;
+import com.nielExpendex.expensesTracker.security.Jwtservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
