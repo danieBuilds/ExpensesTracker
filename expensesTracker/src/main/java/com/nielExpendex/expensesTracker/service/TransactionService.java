97b12f4e42f8c41057125ef47d55d44840956430
danieBuilds <daniebuilds@gmail.com>
2026-07-27 11:45:14 +0100

    new import/annotation

--- expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/TransactionService.java
+++ expensesTracker/src/main/java/com/nielExpendex/expensesTracker/service/TransactionService.java
@@ -7,6 +7,7 @@ import com.nielExpendex.expensesTracker.model.Users;
 import com.nielExpendex.expensesTracker.repository.TransactionRepo;
 import com.nielExpendex.expensesTracker.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
