e3a3cef8d9bf297f4b2a083f78fc5f613e16020c
danieBuilds <daniebuilds@gmail.com>
2026-07-30 15:36:12 +0100

    implemented the use of JpaSpecification, created methods to make it work

---
+++ expensesTracker/src/main/java/com/nielExpendex/expensesTracker/specification/TransactionSpecification.java
@@ -0,0 +1,53 @@
+package com.nielExpendex.expensesTracker.specification;
+
+import com.nielExpendex.expensesTracker.model.Transactions;
+import com.nielExpendex.expensesTracker.model.Users;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Date;
+
+public class TransactionSpecification {
+    public static Specification<Transactions> hasUser(Users user) {
+
+        return (root, query, cb) ->
+                cb.equal(root.get("user"), user);
+
+    }
+    public static Specification<Transactions> hasType(String type) {
+
+        return (root, query, cb) ->
+                cb.equal(root.get("type"), type);
+
+    }
+    public static Specification<Transactions> hasDate(Date date) {
+
+        return (root, query, cb) ->
+                cb.equal(root.get("date"), date);
+
+    }
+    public static Specification<Transactions> hasCategory(Integer id) {
+
+        return (root, query, cb) ->
+                cb.equal(root.get("category").get("id"), id);
+
+    }
+    public static Specification<Transactions> hasKeyword(String keyword) {
+
+        return (root, query, cb) -> {
+
+            String pattern = "%" + keyword.toLowerCase() + "%";
+
+            return cb.or(
+
+                    cb.like(cb.lower(root.get("description")), pattern),
+
+                    cb.like(cb.lower(root.get("type")), pattern),
+
+                    cb.like(cb.lower(root.get("category").get("name")), pattern)
+
+            );
+
+        };
+
+    }
+}
