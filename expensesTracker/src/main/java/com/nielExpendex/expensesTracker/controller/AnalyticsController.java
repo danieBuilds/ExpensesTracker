fa037624ded5100b55c65b8fc14fea0280f7539d
danieBuilds <daniebuilds@gmail.com>
2026-08-04 21:37:56 +0100

    new endpoints

--- expensesTracker/src/main/java/com/nielExpendex/expensesTracker/controller/AnalyticsController.java
+++ expensesTracker/src/main/java/com/nielExpendex/expensesTracker/controller/AnalyticsController.java
@@ -1,6 +1,11 @@
 package com.nielExpendex.expensesTracker.controller;
 
+import com.nielExpendex.expensesTracker.dto.AnalyticsResponse;
+import com.nielExpendex.expensesTracker.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("analytics")
 @RequiredArgsConstructor
 public class AnalyticsController {
+    private final AnalyticsService analyticsService;
 
-
+    @GetMapping("total")
+    public ResponseEntity<AnalyticsResponse> getTotal(){
+        return new ResponseEntity<>(analyticsService.getTotal(), HttpStatus.OK);
+    }
 }
